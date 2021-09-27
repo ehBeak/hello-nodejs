29d9249fb8def910ab4e652927a0a7a77e215af7
eunhee <dmsgml9903@gmail.com>
2021-09-27 17:56:28 +0900

    데이터 보여주기, ejs

--- nodejs/server.js
+++ nodejs/server.js
@@ -4,10 +4,23 @@ const app = express();
 // bodyparser은 express내부에 포함되었음
 const bodyParser = require('body-parser')
 app.use(express.urlencoded({extended : true}));
+app.set('view engine', 'ejs');
 
 // DB 연결
 const MongoClient = require('mongodb').MongoClient;
+
+var db;
+
 MongoClient.connect('mongodb+srv://admin:1234@cluster0.hc9nj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',function (에러, client) {
+    // 연결되면 할 일
+    if(에러) return console.log(에러);
+
+    db = client.db('todoapp');// todoapp이라는 db에 연결
+
+    /*db.collection('post').insertOne({이름 : 'John', 나이 : 20}, function(에러, 결과) { // _id
+        console.log('저장완료');
+    });*/ // post테이블..?에 데이터삽입
+
     // 서버 띄울 포트 번호, 띄운 후 실행 코드
     app.listen(8080, function() {
     // 8080포트로 접속하면 콘솔에 아래 글 띄어주기
@@ -38,6 +51,16 @@ app.get('/write', function(요청,응답) {
 // 응답 받으면 요청에 내용 들어 있음
 app.post('/add', function(요청, 응답) {
     응답.send('전송완료')
-    console.log(요청.body.title);
-    console.log(요청.body.date);
+
+    db.collection('post').insertOne({제목 : 요청.body.title, 날짜 : 요청.body.date}, function(에러, 결과) { // _id
+        console.log('저장완료');
+    }); 
+});
+
+app.get('/list', function(요청, 응답) {
+
+    db.collection('post').find().toArray(function(에러, 결과) {
+        console.log(결과);
+        응답.render('list.ejs', {posts : 결과});
+    }); // 모든 데이터 가져오기
 });
