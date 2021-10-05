f23ddc5701e6d8fa0d0c763e91315040b40502b6
eunhee <dmsgml9903@gmail.com>
2021-10-05 22:58:30 +0900

    3주차 마지막

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 // 서버를 띄우기 위한 기본 셋팅(express 라이브러리)
 const express = require('express');
 const app = express();
@@ -8,6 +10,11 @@ app.set('view engine', 'ejs');
 
 // DB 연결
 const MongoClient = require('mongodb').MongoClient;
+// post, delete요청을 위한 method override
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
+app.use('/public', express.static('public'));
 
 var db;
 
@@ -41,11 +48,13 @@ app.get('/beauty', function(요청, 응답) {
 
 app.get('/', function(요청,응답) {
     // 파일을 보냄
-    응답.sendFile(__dirname + '/index.html')
+    //응답.sendFile(__dirname + '/index.html')
+    응답.render('index.ejs');
 })
 
 app.get('/write', function(요청,응답) {
-    응답.sendFile(__dirname + '/write.html')
+    //응답.sendFile(__dirname + '/write.html')
+    응답.render('write.ejs');
 });
 
 // 응답 받으면 요청에 내용 들어 있음
@@ -88,6 +97,23 @@ app.delete('/delete', function(요청, 응답) {
 })
 
 
-app.get('/detail', function(요청, 응답) {
-    응답.render('detail.ejs', {})
+app.get('/detail/:id', function(요청, 응답) {
+    db.collection('post').findOne({_id : parseInt(요청.params.id)}, function(에러, 결과) {
+        console.log(결과);
+        응답.render('detail.ejs', { data : 결과});
+    })
+    
+})
+
+app.get('/edit/:id', function(요청, 응답) {
+    db.collection('post').findOne({_id : parseInt(요청.params.id)}, function(에러, 결과){
+        응답.render('edit.ejs', {post : 결과});
+    })
+})
+
+app.put('/edit', function(요청, 응답) {
+    db.collection('post').updateOne({ _id : parseInt(요청.body.id) },{ $set : { 제목: 요청.body.title, 날짜: 요청.body.date }},function(에러, 결과) {
+        console.log('수정완료')
+        응답.redirect('/list')
+    })
 })
